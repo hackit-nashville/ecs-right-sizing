83906826e993fe56fd9f65864b56e055e9eb5077
Chris Pruitt <chris.pruitt15@gmail.com>
2020-01-08 10:30:45 -0600

    begin getmetricdata

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module hackit-ecs-right-sizing
 go 1.12
 
 require (
+	github.com/aws/aws-sdk-go v1.15.3
 	github.com/justmiles/ecs-cli v0.0.17
 	github.com/spf13/cobra v0.0.5
 )
--- lib/main.go
+++ lib/main.go
@@ -1,6 +1,20 @@
 package lib
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatch"
+)
+
+var (
+	sess = session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+)
 
 // HelloWorld says hello
 func HelloWorld() {
@@ -9,5 +23,22 @@ func HelloWorld() {
 
 // EstimateReservation looks at your ECS service's historical memory utilization and recommends a memory reservation
 func EstimateReservation(serviceName, clusterName string) (reservation int64) {
+	// var ecs = ecs.New(sess)
+	var cloudwatchService = cloudwatch.New(sess)
+
+	output, err := cloudwatchService.GetMetricData(&cloudwatch.GetMetricDataInput{
+		EndTime: aws.Time(time.Now()),
+		// MetricDataQueries []*MetricDataQuery `type:"list" required:"true"`
+		// NextToken *string `type:"string"`
+		// ScanBy *string `type:"string" enum:"ScanBy"`
+		// StartTime *time.Time `type:"timestamp" required:"true"`
+	})
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println(output)
+
 	return 1
 }
