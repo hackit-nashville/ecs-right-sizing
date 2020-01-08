5202c7182f03c0e7e1e02777f72312899c66c2e5
Reece Lincoln <reece.lincoln@G2Lyticss-MacBook-Pro-2.local>
2020-01-08 14:43:32 -0600

    json load

--- memory_utilization.py
+++ memory_utilization.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import sys
-
+import json
 
 def calculate(data, interval):
-    new_dict = data['MetricDataResults']
+    with open(data) as json_file:
+        temp = json.load(json_file)
+
+    new_dict = temp['MetricDataResults']
     df = pd.DataFrame.from_dict(new_dict)
     timestamps = df.Timestamps[0]
     task_count = df.Values[0]
@@ -26,7 +29,7 @@ def calculate(data, interval):
 
         new_df['Hour'] = new_df['Timestamps'].apply(lambda x: x.hour)
         final_df = new_df.groupby(['Hour'])['MemoryUtilized'].mean().reset_index()
-        final_df.to_csv(path_or_buf='results/hour.csv', sep=',', index=False)
+        final_df.to_csv(path_or_buf='hour.csv', sep=',', index=False)
         with pd.option_context('display.max_rows', None, 'display.max_columns', None):
             print(final_df)
         return
@@ -35,7 +38,7 @@ def calculate(data, interval):
 
         new_df['DOW'] = new_df['Timestamps'].apply(lambda x: x.dayofweek)
         final_df = new_df.groupby(['DOW'])['MemoryUtilized'].mean().reset_index()
-        final_df.to_csv(path_or_buf='results/dow.csv', sep=',', index=False)
+        final_df.to_csv(path_or_buf='dow.csv', sep=',', index=False)
         with pd.option_context('display.max_rows', None, 'display.max_columns', None):
             print(final_df)
         return
@@ -45,7 +48,7 @@ def calculate(data, interval):
         new_df['Hour'] = new_df['Timestamps'].apply(lambda x: x.hour)
         new_df['DOW'] = new_df['Timestamps'].apply(lambda x: x.dayofweek)
         final_df = new_df.groupby(['DOW', ' Hour'])['MemoryUtilized'].mean().reset_index()
-        final_df.to_csv(path_or_buf='results/dow_hourly.csv', sep=',', index=False)
+        final_df.to_csv(path_or_buf='dow_hourly.csv', sep=',', index=False)
         with pd.option_context('display.max_rows', None, 'display.max_columns', None):
             print(final_df)
         return
