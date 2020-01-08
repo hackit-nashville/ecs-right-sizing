f81a1afa876e9a2d58a9d6628273c3823832087b
Reece Lincoln <reece.lincoln@G2Lyticss-MacBook-Pro-2.local>
2020-01-08 14:06:43 -0600

    csv saving

--- memory_utilization.py
+++ memory_utilization.py
@@ -2,7 +2,6 @@ import json
 import pandas as pd
 from enum import Enum
 
-
 class Interval(Enum):
     HOUR = "HOUR",
     DOW = "DOW",
@@ -10,7 +9,6 @@ class Interval(Enum):
 
 
 def calculate(data, interval):
-
     temp = json.loads(data)
 
     new_dict = temp['MetricDataResults']
@@ -36,21 +34,30 @@ def calculate(data, interval):
 
         new_df['Hour'] = new_df['Timestamps'].apply(lambda x: x.hour)
         final_df = new_df.groupby(['Hour'])['MemoryUtilized'].mean().reset_index()
-        return final_df
+        final_df.to_csv(path_or_buf='hour.csv', sep=',', index=False)
+        with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+            print(final_df)
+        return
 
     elif interval.toUpperCase() == Interval.DOW:
 
         new_df['DOW'] = new_df['Timestamps'].apply(lambda x: x.dayofweek)
         final_df = new_df.groupby(['DOW'])['MemoryUtilized'].mean().reset_index()
-        return final_df
+        final_df.to_csv(path_or_buf='dow.csv', sep=',', index=False)
+        with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+            print(final_df)
+        return
 
     elif interval.toUpperCase() == Interval.DOW_HOURLY:
 
         new_df['Hour'] = new_df['Timestamps'].apply(lambda x: x.hour)
         new_df['DOW'] = new_df['Timestamps'].apply(lambda x: x.dayofweek)
-        final_df = new_df.groupby(['DOW',' Hour'])['MemoryUtilized'].mean().reset_index()
-        return final_df
+        final_df = new_df.groupby(['DOW', ' Hour'])['MemoryUtilized'].mean().reset_index()
+        final_df.to_csv(path_or_buf='dow_hourly.csv', sep=',', index=False)
+        with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+            print(final_df)
+        return
 
     else:
         print("Not a valid interval. Please use: HOUR, DOW, or DOW_HOURLY")
-        return None
+        return
