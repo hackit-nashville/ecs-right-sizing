321e2d9008a65484387e1e74456dfbda1e310249
Reece Lincoln <reece.lincoln@G2Lyticss-MacBook-Pro-2.local>
2020-01-08 14:31:38 -0600

    remove

--- memory_utilization.py
+++ memory_utilization.py
@@ -1,17 +1,9 @@
-import json
 import pandas as pd
-from enum import Enum
-
-class Interval(Enum):
-    HOUR = "HOUR",
-    DOW = "DOW",
-    DOW_HOURLY = "DOW_HOURLY"
+import sys
 
 
 def calculate(data, interval):
-    temp = json.loads(data)
-
-    new_dict = temp['MetricDataResults']
+    new_dict = data['MetricDataResults']
     df = pd.DataFrame.from_dict(new_dict)
     timestamps = df.Timestamps[0]
     task_count = df.Values[0]
@@ -30,37 +22,38 @@ def calculate(data, interval):
 
     new_df = data.drop(columns=['RunningTaskCount', 'MemoryReserved'])
 
-    if interval.toUpperCase() == Interval.HOUR:
+    if interval.upper() == 'HOUR':
 
         new_df['Hour'] = new_df['Timestamps'].apply(lambda x: x.hour)
         final_df = new_df.groupby(['Hour'])['MemoryUtilized'].mean().reset_index()
-        final_df.to_csv(path_or_buf='hour.csv', sep=',', index=False)
-        with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+        final_df.to_csv(path_or_buf='results/hour.csv', sep=',', index=False)
+        with pd.option_context('display.max_rows', None, 'display.max_columns', None):
             print(final_df)
         return
 
-    elif interval.toUpperCase() == Interval.DOW:
+    elif interval.upper() == 'DOW':
 
         new_df['DOW'] = new_df['Timestamps'].apply(lambda x: x.dayofweek)
         final_df = new_df.groupby(['DOW'])['MemoryUtilized'].mean().reset_index()
-        final_df.to_csv(path_or_buf='dow.csv', sep=',', index=False)
-        with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+        final_df.to_csv(path_or_buf='results/dow.csv', sep=',', index=False)
+        with pd.option_context('display.max_rows', None, 'display.max_columns', None):
             print(final_df)
         return
 
-    elif interval.toUpperCase() == Interval.DOW_HOURLY:
+    elif interval.upper() == 'DOW_HOURLY':
 
         new_df['Hour'] = new_df['Timestamps'].apply(lambda x: x.hour)
         new_df['DOW'] = new_df['Timestamps'].apply(lambda x: x.dayofweek)
         final_df = new_df.groupby(['DOW', ' Hour'])['MemoryUtilized'].mean().reset_index()
-        final_df.to_csv(path_or_buf='dow_hourly.csv', sep=',', index=False)
-        with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+        final_df.to_csv(path_or_buf='results/dow_hourly.csv', sep=',', index=False)
+        with pd.option_context('display.max_rows', None, 'display.max_columns', None):
             print(final_df)
         return
 
     else:
         print("Not a valid interval. Please use: HOUR, DOW, or DOW_HOURLY")
         return
+
 
 if __name__ == '__main__':
     calculate(*sys.argv[1:])
